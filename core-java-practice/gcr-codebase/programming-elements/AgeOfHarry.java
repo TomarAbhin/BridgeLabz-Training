d349729281b6a5e9d71a52cfb570236b17a7fbf1
TomarAbhin <tomar.abhinav3107@gmail.com>
2025-12-18 11:08:22 +0530

    [Abhinav Tomar] Added 3 java practice programs

---
+++ core-java-practice/gcr-codebase/programming-elements/AgeOfHarry.java
@@ -0,0 +1,9 @@
+public class AgeOfHarry {
+    public static void main(String[] args) {
+        int byear = 2000; // input birth year
+        int cyear = 2024; //input current year
+        int age = cyear - byear;
+        System.out.println("harry's age is: " + age);
+    }
+    
+}
---
+++ core-java-practice/gcr-codebase/programming-elements/SamAvaerageMarks.java
@@ -0,0 +1,11 @@
+public class SamAvaerageMarks {
+    public static void main(String[] args) {
+        int mathmarks = 94; // input math marks
+        int physicsmarks = 95; // input physics marks
+        int chemistrymarks = 96; // input chemistry marks
+        int total = mathmarks + physicsmarks + chemistrymarks; // calculate total marks
+        double averagemarks = total / 3.0;
+        System.out.println("Sam's average marks: " + averagemarks);
+    }
+    
+}
