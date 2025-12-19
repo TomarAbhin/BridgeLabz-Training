68f77ade542971cc5db331f705eb325a711ec803
TomarAbhin <tomar.abhinav3107@gmail.com>
2025-12-19 22:34:18 +0530

    [Abhinav Tomar] Refactor. fixed the OddEven code

--- core-java-practice/gcr-codebase/control-flows/OddAndEven.java
+++ core-java-practice/gcr-codebase/control-flows/OddAndEven.java
@@ -3,12 +3,14 @@ public class OddAndEven {
     public static void main(String[] args){
         Scanner s=new Scanner(System.in);
         int num=s.nextInt(); //input the number
-        for(int i=1;i<=num;i++){ //loop from 1 to number
-            if(i%2==0){ //check if the number is even
-                System.out.println(i+" is even number");
-            }
-            else{ //if the number is not even then it is odd
-                System.out.println(i+" is odd number");
+        if(num>0){
+            for(int i=1;i<=num;i++){ //loop from 1 to number
+                if(i%2==0){ //check if the number is even
+                    System.out.println(i+" is even number");
+                }
+                else{ //if the number is not even then it is odd
+                    System.out.println(i+" is odd number");
+                }
             }
         }
     }
