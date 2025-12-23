676f73a16a7cd574f81008c00843d411bc3ab069
TomarAbhin <tomar.abhinav3107@gmail.com>
2025-12-23 16:11:06 +0530

    [Abhinav Tomar] Add. added extra bulit-in function java problems

---
+++ core-java-practice/gcr-codebase/extras/DateComparision.java
@@ -0,0 +1,23 @@
+ import java.time.LocalDate;
+import java.util.Scanner;
+public class DateComparision {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter first date (YYYY-MM-DD): ");
+        LocalDate date1 = LocalDate.parse(s.next());    //input the first date
+
+        System.out.print("Enter second date (YYYY-MM-DD): ");
+        LocalDate date2 = LocalDate.parse(s.next());    // input the second date
+
+        // Compare the two dates
+        if (date1.isBefore(date2)) {
+            System.out.println("First date is before the second date.");
+        } else if (date1.isAfter(date2)) {
+            System.out.println("First date is after the second date.");
+        } else if (date1.isEqual(date2)) {
+            System.out.println("Both dates are the same.");
+        }
+    }
+}
+
+
