a8f9a0c80af87fa90fab828f05014cfa59c2c6ef
TomarAbhin <tomar.abhinav3107@gmail.com>
2026-01-28 21:44:16 +0530

    [Abhinav Tomar] Refactor. fixed the code.

--- core-java-practice/scenario-based/logistics/Main.java
+++ core-java-practice/scenario-based/logistics/Main.java
@@ -20,8 +20,17 @@ public class Main {
 		    System.out.println("Brick price : " + bt.getBrickPrice());
 		    System.out.println("Vehicle for transport : " + bt.vehicleSelection());
 		    System.out.println("Total charge : " + bt.calculateTotalCharge());
+		}else if(gt instanceof TimberTransport) {
+			TimberTransport tt=(TimberTransport) gt;
+		 	System.out.println("\nTransporter id : " + tt.getTransportId());
+		    System.out.println("Date of transport : " + tt.getTransportDate());
+			System.out.println("Rating of the transport : " + tt.getTransportRating());			    System.out.println("Timber Length: " + tt.getTimberLength());
+		    System.out.println("Timber Radius : " + tt.getTimberRadius());
+		    System.out.println("Timber Type : " + tt.getTimberType());
+		    System.out.println("Timber price : " + tt.getTimberPrice());
+			System.out.println("Vehicle for transport : " + tt.vehicleSelection());
+			System.out.println("Total charge : " + tt.calculateTotalCharge());
 		}
-		
 		s.close();
 	}
 
